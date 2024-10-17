8be37303b7e1a51323863f11d3308c5d4309350a
SerhatAker <81346603+serhataker@users.noreply.github.com>
2024-10-17 21:26:05 +0300

    Mail is requiered

--- Request/RequestView.cs
+++ Request/RequestView.cs
@@ -8,6 +8,7 @@ namespace IdentityDataProtection.Request
     public class RequestView
     {
         [Required(ErrorMessage = "Username is required")]
+        [EmailAddress]
         [StringLength(50, MinimumLength = 3, ErrorMessage = "The username must be between 3 and 50 characters.")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Password is required.")]
